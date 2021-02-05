bbf477233a861055b921c5245fa5b1735dd8e6b7
Sy McNeely <craigeire@gmail.co>
2021-02-05 14:01:14 -0500

    commented code oops

--- Time to Kill/Assets/Scripts/playerMovement.cs
+++ Time to Kill/Assets/Scripts/playerMovement.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Created by: Kiera McNeely
+//Date Created: 2021-02-05
+//Date Edited: 2021-02-05
+
 public class playerMovement : MonoBehaviour
 {
+   
     public Rigidbody2D rigidbody;
     public float movementSpeed = 5f;
     public float h;
@@ -11,18 +16,21 @@ public class playerMovement : MonoBehaviour
 
     void Start()
     {
+        //get access to rigid body
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
+        //create input for both x and y directions
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
+        //move the player at specified move speed
         rigidbody.velocity = new Vector2(h * movementSpeed, v * movementSpeed);
     }
 }
