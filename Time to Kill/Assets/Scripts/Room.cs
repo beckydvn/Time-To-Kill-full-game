f92d671b8ff8061cf683717bd9abc99c2b0bbe18
Rebecca De Venezia <rebeccaleahdevenezia@gmail.com>
2021-02-15 01:07:59 -0500

    almost all cameras working!

---
+++ Time to Kill/Assets/Scripts/Room.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject virtualCamera;
+    public virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        //whenever the player enters the camera region, set the camera to active
+        if (other.CompareTag("Player"))
+        {
+            virtualCamera.SetActive(true);
+            Debug.Log(virtualCamera + " turned on");
+        }
+    }
+    public virtual void OnTriggerExit2D(Collider2D other)
+    {
+        //turn the camera off when the player leaves the region
+        if (other.CompareTag("Player"))
+        {
+            virtualCamera.SetActive(false);
+            Debug.Log(virtualCamera + " turned off");
+        }
+    }
+
+    private void Update()
+    {
+        //Debugging purposes
+        //Debug.Log(virtualCamera + " at " + virtualCamera.transform.position);
+    }
+
+    private void Start()
+    {
+        //each camera should be off initially
+        virtualCamera.SetActive(false);
+    }
+
+
+}
