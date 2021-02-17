3cbb3f4d28749264edb8e9b4c2c2a77fcb266cd7
Sy McNeely <craigeire@gmail.co>
2021-02-16 19:48:40 -0500

    added countdown

---
+++ Time to Kill/Assets/Scripts/countdownTimer.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdownTimer : MonoBehaviour
+{
+
+
+    public Text timerText;
+
+    private bool isTimer;
+    private float timeLeft = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        isTimer = true;
+    }
+
+    void displayTime(float t)
+    {
+        TimeSpan r = TimeSpan.FromSeconds(t);
+
+        timerText.text = r.ToString("mm':'ss':'ff");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimer)
+        {
+           
+
+            if(timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                displayTime(timeLeft);
+            }
+            else
+            {
+                timeLeft = 0;
+                isTimer = false;
+            }
+            
+        }
+    }
+}
